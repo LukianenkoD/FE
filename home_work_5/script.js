7f3c150abbe08a7dfaa445f875ed83d8f326270f
LukianenkoD <118936806+LukianenkoD@users.noreply.github.com>
2023-02-23 18:52:59 +0100

    Add files via upload

---
+++ home_work_5/script.js
@@ -0,0 +1,26 @@
+// Hw
+// -1-
+let number = prompt("Enter number:");
+console.log(number**2);
+// -2-
+let number1 = prompt("Enter number:");
+if (number1%2) {
+
+console.log("uneven")
+
+}
+else if (number1%3) {
+    console.log("number even ")
+}
+else {
+     console.log("number = 0")
+}
+// -3-
+let number2 = prompt("Enter number:");
+
+if (number2 = +number2){
+    console.log("Это значение можно преобразовать в число ")
+}
+else {
+    console.log("Это значение преобразовать в число не получится. Получится " + number2)
+}
